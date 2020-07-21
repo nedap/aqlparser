81bbbae7d58df7f0b3c0695f8c334a28ff1dafa9
Michael Böckers <michael.bockers@nvc4164.nedap.local>
2020-07-21 14:41:18 +0200

    added getParameterKey to lookup

--- src/main/java/com/nedap/healthcare/aqlparser/model/Lookup.java
+++ src/main/java/com/nedap/healthcare/aqlparser/model/Lookup.java
@@ -59,7 +59,7 @@ public class Lookup {
     }
 
     public String getParameterKey(PrimitiveOperand primitiveOperand) {
-        return parameter.entrySet().stream().filter(entry -> primitiveOperand.getValue().equals(entry.getValue()))
+        return parameter.entrySet().stream().filter(primitiveOperand::equals).map(Map.Entry::getKey).findFirst().get();
     }
 
 }
